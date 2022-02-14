d312f6ce832b730c652644708944df3ce68ebcda
Gregorio Lixandrão <contato@greglixandrao.com>
2022-02-14 19:43:51 -0300

    add express cors make root route

---
+++ src/shared/http/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+const port = 3333;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(port, () => {
+  console.log(`Server started on ${port} 🎇🎇🎇`);
+});
