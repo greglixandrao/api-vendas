f3bd7db44711af6ff42c925959db30aaadde013f
Gregorio Lixandrão <contato@greglixandrao.com>
2022-03-02 13:54:25 -0300

    make CreateSession login

---
+++ src/modules/users/services/CreateSessionsService.ts
@@ -0,0 +1,34 @@
+import AppError from '@shared/errors/AppError';
+import { compare, hash } from 'bcryptjs';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/users';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+// interface IResponse {
+//   user: User;
+// }
+
+class CreateSessionsService {
+  public async execute({ email, password }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const user = await usersRepository.findByEmail(email);
+    if (!user) {
+      throw new AppError('Incorrect email/password.', 401);
+    }
+
+    const passwordConfirmed = await compare(password, user.password);
+
+    if (!passwordConfirmed) {
+      throw new AppError('Incorrect email/password.', 401);
+    }
+
+    return user;
+  }
+}
+
+export default CreateSessionsService;
