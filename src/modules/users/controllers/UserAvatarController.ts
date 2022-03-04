5b8da1f255a9f0fac2d47c2d058d661389c377f1
Gregorio Lixandrão <contato@greglixandrao.com>
2022-03-04 12:06:12 -0300

    cleaning UserAvatarController

--- src/modules/users/controllers/UserAvatarController.ts
+++ src/modules/users/controllers/UserAvatarController.ts
@@ -13,19 +13,3 @@ export default class UserAvatarController {
     return response.json(user);
   }
 }
-
-// import { Request, Response } from 'express';
-// import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
-
-// export default class UserAvatarController {
-//   public async update(request: Request, response: Response): Promise<Response> {
-//     const updateAvatar = new UpdateUserAvatarService();
-
-//     const user = updateAvatar.execute({
-//       user_id: request.user.id,
-//       avatarFilename: request.file.filename,
-//     });
-
-//     return response.json(user);
-//   }
-// }
