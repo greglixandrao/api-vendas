be215a6e138038acecb971206574de4a5e3094fd
Gregorio Lixandrão <contato@greglixandrao.com>
2022-03-04 12:05:01 -0300

    add static route for files

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -6,13 +6,14 @@ import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import { errors } from 'celebrate';
 import '@shared/typeorm';
+import uploadConfig from '@config/upload';
 const port = 3333;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 app.use(errors());
 
