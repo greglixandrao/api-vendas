b391af11e4a3215c4db9592a48f21044c75c4a4b
Gregório Lixandrão <contato@greglixandrao.com>
2022-03-08 19:38:07 -0300

    add password route to principal route

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,6 +1,7 @@
 import productsRoutes from '@modules/products/routes/products.routes';
 import sessionsRoutes from '@modules/users/routes/sessions.routes';
 import usersRoutes from '@modules/users/routes/users.routes';
+import passwordRouter from '@modules/users/routes/password.routes';
 import { Router } from 'express';
 
 const routes = Router();
@@ -8,5 +9,6 @@ const routes = Router();
 routes.use('/products', productsRoutes);
 routes.use('/users', usersRoutes);
 routes.use('/sessions', sessionsRoutes);
+routes.use('/password', passwordRouter);
 
 export default routes;
