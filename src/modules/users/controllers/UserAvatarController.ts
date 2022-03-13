aa34e490f41892a7280845b0d859fa1e0dcadf98
Gregorio Lixandrão <contato@greglixandrao.com>
2022-03-13 17:43:22 -0300

    update UserAvatarController

--- src/modules/users/controllers/UserAvatarController.ts
+++ src/modules/users/controllers/UserAvatarController.ts
@@ -1,6 +1,6 @@
-import { instanceToInstance } from 'class-transformer';
 import { Request, Response } from 'express';
 import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+import { instanceToInstance } from 'class-transformer';
 
 export default class UserAvatarController {
   public async update(request: Request, response: Response): Promise<Response> {
