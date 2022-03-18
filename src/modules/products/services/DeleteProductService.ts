e5ab98a864f78c225117aa1d2d9269acf9b7165c
Gregorio Lixandrão <contato@greglixandrao.com>
2022-03-17 21:18:04 -0300

    implement invalidate cache to DeleteProductService

--- src/modules/products/services/DeleteProductService.ts
+++ src/modules/products/services/DeleteProductService.ts
@@ -1,19 +1,22 @@
 import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
-import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
-
+import ProductsRepository from '../typeorm/repositories/ProductsRepository';
+import RedisCache from '@shared/cache/RedisCache';
 interface IRequest {
   id: string;
 }
 
 class DeleteProductsService {
   public async execute({ id }: IRequest): Promise<void> {
-    const productsRepository = getCustomRepository(ProductRepository);
+    const productsRepository = getCustomRepository(ProductsRepository);
 
     const product = await productsRepository.findOne(id);
     if (!product) {
       throw new AppError('Product not found.');
     }
+    const redisCache = new RedisCache();
+
+    await redisCache.invalidate('api-vendas-PRODUCTS_LIST');
 
     await productsRepository.remove(product);
   }
